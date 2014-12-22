92fd286277710cbdeb6de5bc8ab6ed720e3eb54d
Vince Well <vince.i.well@gmail.com>
2014-12-22 23:33:13 +0800

    C++

--- C++/5.10 class template.cpp
+++ C++/5.10 class template.cpp
@@ -1,3 +1,37 @@
+/**
+ * 
+ */
+template <typename T1, typename T2, int initial_preexist = 100>
+class Precede{
+	private:
+		static int preexist;
+	public:
+		T1 precedence(T1 x){return x;}
+		void precedent(T2 z);
+};
+
+template <typename T1, typename T2, int initial_preexist>
+int Precede<T1,T2,initial_preexist>::preexist = initial_preexist;    // initialize static preexist;
+
+template <typename T1, typename T2, int initial_preexist>
+void Precede<T1,T2,initial_preexist>::precedent(T2 z){
+	cout << "precedent: " << z << endl;
+}
+
+template <typename T1, typename T2, int initial_preexist>
+void convey(Precede<T1,T2,initial_preexist> p){
+	cout << p.precedence(initial_preexist) << endl;
+}
+
+
+Precede<int, const char *, 400> p;
+const char * precession = "Vince-Well";
+p.precedent(precession);
+convey(p);   // 400
+
+
+
+
 template <class Type>
 class Fossil
 {
@@ -97,4 +131,62 @@ private:
 Arr< Stack<int> > arr;
 
 
+
+
+/* You should use templates if you need functions that apply the same algorithm to a variety of
+types. If you aren’t concerned with backward compatibility and can put up with the effort of
+typing a longer word, you can use the keyword "typename" rather than "class" when you
+declare type parameters. */
+
+template <typename T> void func(T &a);   // implicit instantiation
+
+
+/* Originally,using implicit instantiation was the only way the compiler generated func-
+tion definitions from templates,but now C++ allows for explicit instantiation.That means
+you can instruct the compiler to create a particular instantiation—for example,
+Swap<int>()—directly.The syntax is to declare the particular variety you want,using the
+<>notation to indicate the type and prefixing the declaration with the keyword
+template: */
+
+template void Swap<int>(int, int);  // explicit instantiation
+template <> void Swap<int>(int &, int &);  // explicit specialization
+template <> void Swap(int &, int &);       // explicit specialization
+
+/* The difference is that these last two declarations mean “Don’t use the Swap() template
+to generate a function definition.Instead,use a separate,specialized function definition
+explicitly defined for the int type.”These prototypes have to be coupled with their own
+function definitions.The explicit specialization declaration has <> after the keyword tem-
+plate,whereas the explicit instantiation omits the <>.
+
+It is an error to try to use both an explicit instantiation and an explicit specia
+same type(s) in the same file,or,more generally,the same translation unit. */
+
+template <class T>
+T Add(T a, T b)    // pass by value
+{
+return a + b;
+}
+...
+int m = 6;
+double x = 10.2;
+cout << Add<double>(x, m) << endl;  // explicit instantiation
+
+/* The template would fail to match the function call Add(x, m)because the te
+expects both function arguments to be of the same type.But using Add<double>
+forces the type doubleinstantiation,and the argument mis type cast to type doub
+match the second parameter of the Add<double>(double, double)function. */
+
+
+void Swap(T &a, T &b); 
+
+/* This generates an explicit instantiation for type double.Unfortunately,in this case,the
+code won’t work because the first formal parameter,being type double &,can’t refer to
+the type int variable m.
+Implicit instantiations,explicit instantiations,and explicit specializations collectively are
+termed specializations.What they all have in common is that they represent a function defi-
+nition that uses specific types rather than one that is a generic description.
+The addition of the explicit instantiation led to the new syntax of using template and
+template <>prefixes in declarations to distinguish between the explicit instantiation and
+the explicit specialization.As in many other cases,the cost of doing more is more syntax
+rules.The following fragment summarizes these concepts: */
 
--- C++/5.9 inheritance and virtual.cpp
+++ C++/5.9 inheritance and virtual.cpp
@@ -1,16 +1,110 @@
+/**
+ * DerivedClass: public BaseClass ==>
+ * DerivedClass: private BaseClass ==> public/protected/private turns to private
+ * DerivedClass: protected BaseClass ==> public/protected turns to protected
+ 
+ ** Sequence to call BaseClass's construct/destruct function   *****************
+ * 	1. Calls default BaseClass's construct method first. If there are more than 
+		one BaseClass, call them via the seq. of declared.
+		class Derived: public Base1, public Base2{    	// via here
+			Derived(int x): Base2(x), Base1(x);       	// not here
+		};
+	2. Calls BaseClass's construct method in Derived Class. Call them via the 
+		sequence of declared.
+		class Derived{
+			Base5 b5;
+			Base6 b6;
+		};
+	3. Calls Derived class's construct method
+	7. destruct of 3
+	8. destruct of 2
+	9. destruct of 1
+	Sumary:
+		class Base5{
+				Base4 b4;
+		};
+		class Derived: public Base1, public Base2, public Base3{	// VIA HERE
+				Base5 b5;  			 // not declared in inheritance
+			public:
+				Derived(int i):Base3(i+1), Base1(i), Base2{}   		// NOT HERE
+				~Derived(){}
+		};
+		==> 
+		Base1::Base1() --> Base2::Base2() --> Base3::Base3() 
+		--> Base1::Base1(i) --> Base2::Base2(i) --> Base3::Base3(i+1)
+		--> Base4::Base4() --> Base5::Base5()
+		--> Derived:Derived(i)
+		--> Derived::~Derived()
+		--> Base5::~Base5() --> Base4::~Base4()
+		--> Base3::~Base3() --> Base2::~Base2() --> Base1::~Base1()
+ */
+
+class Recollect{
+	private:
+		int x;
+	public:
+		Recollect(){cout << "Recollect::Recollect()" << endl;}
+		Recollect(int j){
+			x=j; 
+			cout << "Recoolect::Recollect(int) :" << j << endl;
+		}
+		void setX(int i):x(i){}
+		void getX(){return x;}
+		bool checkX(int y){return x == y;}
+};
+ 
+ 
+class Remembrance : private Recollect    // private inherit
+{
+	private:
+	public:
+		Remebrance(){cout << "Remembrance::Remembrance()" << endl;}
+		Remebrance(int j): Recollect(j){}  // set its parent's construct
+		Recollect::setX;    // turn private public
+		Recollect::getX;
+};
+
+void recall(Recollect r){
+	cout << r.getX() << endl;
+}
+
+/**
+ 
+  Remebrance -[INHERITE]-> Recollect
+  Recollect::Recollect() --> Remembrance::Remembrance()
+  --> Remembrance::~Remembrance() --> Recollect::~Recollect()
+ 
+ 
+ */
+Remembrance r;
+r.setX(100);  // ok
+r.getX(); // 100
+// r.checkX(100)   false,  it's a private method
+ 
+/**
+ *
+ */
+Remebrance r2(100);
+recall(r2);
+ 
+ 
+ 
+ 
+
 class Copper
 {
 public:
 	void smelt(){ cout << "smelt" << endl; }
 	virtual void forge(){ cout << "forge" << endl; }
 };
+
 class Brass :   public Copper, 
                 protected std::string, 
                 private std::valarray<int>
 {
 public:
     using std::valarray<int>::min;
-	void smelt(){ cout << "re-smelt" << endl; }
+	void smelt(){ cout << "re-smelt" << endl; }    // redefine smelt()
 	virtual void forge(){ cout << "re-forge" << endl; }
 };
 
@@ -20,6 +114,8 @@ public:
 Copper * copper = new Brass();
 copper->smelt();   // smelt
 copper->forge();   // re-smelt
+
+
 
 
 
