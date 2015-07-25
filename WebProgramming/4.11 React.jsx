2f3e6b54fa6603abb0c516fe2f57164f9f0ddcc3
Lef <lef.well@gmail.com>
2015-07-25 18:50:34 +0800

    event handle in React js

--- WebProgramming/4.11 React.jsx
+++ WebProgramming/4.11 React.jsx
@@ -1,4 +1,37 @@
 /**
+ * @warning distinguish the `this` is map
+ * this.[state|prop].vars.map(function(item, i){}, hanle_this = each item);
+ */
+handle: function(){},
+render: function(){
+  var this_ = this;
+  return (
+    <div>{
+      this.state.items.map(function(item, i){
+        var bound_handle = this_.handle.bind(item, i);    // notice `this_`
+        return (
+          <a href="javascript:void(0)" onClick=bound_handle>Lef</a>
+          <a href="javascript:void(0)" onClick=this_.handle>Well</a>
+        );
+      });
+    }</div>
+  );
+}
+
+render: function(){
+  var list = this.props.items.map(function(item, i){
+    var bound_handle = this.handle.bind(item, i);
+    return (
+      <a href="javascript:void(0)" onClick=bound_handle>Lef</a>
+      <a href="javascript:void(0)" onClick=this.handle>Well</a>
+    );
+  }, this);     // assign interior `this` to outter `this`
+  return (
+    <div>{list}</div>
+  );
+}
+
+/**
  * class hanle
  */
 render: function(){
@@ -11,3 +44,56 @@ render: function(){
   // <div class="p"> or <div class="p a"> or  <div class="p a mg"> ...
   return <div className={cls}></div>
 }
+
+/**
+ * @see
+ *  https://facebook.github.io/react/tips/expose-component-functions.html
+ *  http://facebook.github.io/react/docs/events.html
+ * Events:
+ *  SyntheticEvent
+ *  Clipboard Events: onCopy onCut onPaste
+ *    DomDataTransfer clipboardData
+ *  Keyboard Events:  onKeyDown onKeyPress onKeyUp
+ *    boolean:  altKey ctrlKey metaKey repeat shiftKey
+ *    Number:   charCode keyCode location which
+ *    function: getModifierState(key)
+ *    String:   key locale
+ *  Focus Events: onFocus onBlur
+ *    DOMEventTarget relatedTarget
+ *  Form Events: onChange onInput onSubmit
+ *  Mouse Events:
+ *    onClick onContextMenu onDoubleClick onDrag onDragEnd onDragEnter onDragExit
+ *    onDragLeave onDragOver onDragStart onDrop onMouseDown onMouseEnter onMouseLeave
+ *    onMouseMove onMouseOut onMouseOver onMouseUp
+ *      boolean altKey ctrlKey metaKey shiftKey
+ *      Number button buttons clientX clientY pageX pageY screenX screenY
+ *      function getModifierState(key)
+ *      DOMEventTarget relatedTarget
+ *  Touch Evnets: onTouchCancel onTouchEnd onTouchMove onTouchStart
+ *  UI Events: onScrool
+ *    Number detail
+ *    DOMAbstractView view
+ *  Wheel Events: onWheel
+ *    Number: daltaMode deltaX deltaY deltaZ
+ */
+var Rcc = React.createClass({
+  getInitialState: function(){},
+  handleClick: function(e){
+  
+  },
+  render: function(){
+    return (
+      <div>
+        {
+          this.state.items.map(function(item, i){
+            var bound_click = this.handleClick.bind(this, i);
+            return (
+              <div onClick={bound_click}></div>
+              <div onClick={this.handleClick}></div>
+            ); 
+          }, this)
+        }
+      </div>
+    );
+  }
+});
