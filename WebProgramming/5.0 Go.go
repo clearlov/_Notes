8a15b7c2a1f16f064a0d8ee936305eb8bb082e45
LefWell <lef.well@gmail.com>
2015-10-11 23:27:13 +0800

    Golang variables

---
+++ WebProgramming/5.0 Go.go
@@ -0,0 +1,30 @@
+package main
+import "fmt"
+
+var (
+  intense int = 100
+  interfere = 100
+  prime *int
+  projection string
+  bison [10]int
+  relief []int
+)
+
+inter := 5
+
+
+interfere, inter = inter, interfare       // now interfere=5, inter=100
+
+var dominant struct {
+  adequate map[string]int       // key:string  value:int
+}
+
+var trap func(trace int) string
+
+
+func main(){
+  fmt.Println("Go! Lef Well");
+}
+
+// sh$ go build tutorial.go             --> build a tutorial file
+// sh$ ./tutorial                       --> Go! Lef Well
